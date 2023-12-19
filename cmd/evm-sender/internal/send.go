09e702392ca3e4fe6f90405bd6b244b1424c262b
Ducca <64695363+duccas@users.noreply.github.com>
2023-12-20 00:57:12 +0300

    add send-back option

--- cmd/evm-sender/internal/send.go
+++ cmd/evm-sender/internal/send.go
@@ -2,20 +2,17 @@ package internal
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/staketab/evm-sender/cmd/evm-sender/internal/var"
 	"github.com/staketab/evm-sender/cmd/evm-sender/pkg/func"
 	"log"
 	"math/big"
 	"math/rand"
+	"strconv"
 	"time"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func Rand(min *big.Int, max *big.Int) *big.Int {
@@ -26,57 +23,37 @@ func Rand(min *big.Int, max *big.Int) *big.Int {
 	return randNum
 }
 
-func SendTx() {
+func SendRangeTx() {
 	config, err := ff.ReadConfigs()
 	if err != nil {
 		vars.ErrorLog.Fatal(err)
 	}
 
-	client, err := ethclient.Dial(config.Default.Rpc)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	privateKey, err := crypto.HexToECDSA(config.Default.PrivateKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		log.Fatal("error casting public key to ECDSA")
-	}
-
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	//value := big.NewInt(config.Default.Value) // in wei
 	gasLimit := config.Default.GasLimit // in units
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	toAddress := common.HexToAddress(config.Default.Recipient)
-
 	memo := config.Default.Memo
 	data := hex.EncodeToString([]byte(memo))
-
-	chainID, err := client.NetworkID(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	min := big.NewInt(config.Default.Min)
 	max := big.NewInt(config.Default.Max)
 
+	client, privateKey, nonce, gasPrice, chainID, err := ff.InitializeEthereumClientDefault(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	inTimeSeconds, err := strconv.Atoi(config.Default.InTime)
+	if err != nil {
+		log.Fatal(err)
+	}
 	for {
 		for i := 0; i < config.Default.TxCount; i++ {
-			value := Rand(min, max)
+			var value *big.Int
+			if config.Default.Value != 0 {
+				value = big.NewInt(config.Default.Value)
+			} else {
+				value = Rand(min, max)
+			}
 			tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, []byte(data))
 			signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
 			if err != nil {
@@ -92,6 +69,48 @@ func SendTx() {
 			nonce++
 		}
 
-		time.Sleep(60 * time.Second)
+		time.Sleep(time.Duration(inTimeSeconds) * time.Second)
+	}
+}
+
+func SendBackTx() {
+	config, err := ff.ReadConfigs()
+	if err != nil {
+		vars.ErrorLog.Fatal(err)
+	}
+	time.Sleep(10 * time.Second)
+	if config.SendBack.Enable {
+		value := big.NewInt(config.SendBack.Value) // in wei
+		gasLimit := config.SendBack.GasLimit       // in units
+		toAddress := common.HexToAddress(config.SendBack.Recipient)
+		memo := config.SendBack.Memo
+		data := hex.EncodeToString([]byte(memo))
+
+		client, privateKey, nonce, gasPrice, chainID, err := ff.InitializeEthereumClientSendBack(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+		inTimeSeconds, err := strconv.Atoi(config.SendBack.InTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for {
+			for i := 0; i < config.SendBack.TxCount; i++ {
+				tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, []byte(data))
+				signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				err = client.SendTransaction(context.Background(), signedTx)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Printf("Tx back: %s\n", signedTx.Hash().Hex())
+				nonce++
+			}
+			time.Sleep(time.Duration(inTimeSeconds) * time.Second)
+		}
 	}
 }
--- cmd/evm-sender/pkg/func/func.go
+++ cmd/evm-sender/pkg/func/func.go
@@ -1,10 +1,16 @@
 package ff
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"errors"
 	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pelletier/go-toml"
 	"github.com/staketab/evm-sender/cmd/evm-sender/internal/controller"
 	"github.com/staketab/evm-sender/cmd/evm-sender/internal/var"
+	"math/big"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -37,12 +43,23 @@ func CreateConfigFile(path string) error {
 rpc = ""
 private_key = ""
 recipient = ""
-value = 1000000
+fixedValue = 0
 gas_limit = 22000
 memo = "From Staketab with LOVE!"
-txcount = 3
+txCount = 3
+inTime = "60"
 min = 1000000000000000000
 max = 9000000000000000000
+
+[SEND-BACK]
+enable = false
+private_key = ""
+recipient = ""
+fixedValue = 1000000000000000000
+gas_limit = 22000
+memo = "From Staketab with LOVE!"
+txCount = 1
+inTime = "60"
 `)
 
 	err2 := os.WriteFile(filePath, content, 0644)
@@ -72,4 +89,76 @@ func ReadConfigs() (controller.Config, error) {
 	}
 
 	return config, nil
+}
+
+func InitializeEthereumClientDefault(config controller.Config) (*ethclient.Client, *ecdsa.PrivateKey, uint64, *big.Int, *big.Int, error) {
+	client, err := ethclient.Dial(config.Default.Rpc)
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	privateKey, err := crypto.HexToECDSA(config.Default.PrivateKey)
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, nil, 0, nil, nil, errors.New("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	return client, privateKey, nonce, gasPrice, chainID, nil
+}
+
+func InitializeEthereumClientSendBack(config controller.Config) (*ethclient.Client, *ecdsa.PrivateKey, uint64, *big.Int, *big.Int, error) {
+	client, err := ethclient.Dial(config.Default.Rpc)
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	privateKey, err := crypto.HexToECDSA(config.SendBack.PrivateKey)
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, nil, 0, nil, nil, errors.New("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return nil, nil, 0, nil, nil, err
+	}
+
+	return client, privateKey, nonce, gasPrice, chainID, nil
 }
