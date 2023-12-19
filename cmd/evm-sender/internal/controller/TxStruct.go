ae9f8556ff587ab952c97e23f8e9e7114c592b2a
Ducca <64695363+duccas@users.noreply.github.com>
2023-12-20 00:57:23 +0300

    add struct

--- cmd/evm-sender/internal/controller/TxStruct.go
+++ cmd/evm-sender/internal/controller/TxStruct.go
@@ -1,17 +1,30 @@
 package controller
 
 type Config struct {
-	Default DefaultConfig `toml:"DEFAULT"`
+	Default  DefaultConfig  `toml:"DEFAULT"`
+	SendBack SendBackConfig `toml:"SEND-BACK"`
 }
 
 type DefaultConfig struct {
 	Rpc        string `toml:"rpc"`
 	PrivateKey string `toml:"private_key"`
 	Recipient  string `toml:"recipient"`
-	Value      int64  `toml:"value"`
+	Value      int64  `toml:"fixedValue"`
 	GasLimit   uint64 `toml:"gas_limit"`
 	Memo       string `toml:"memo"`
-	TxCount    int    `toml:"txcount"`
+	TxCount    int    `toml:"txCount"`
+	InTime     string `toml:"inTime"`
 	Min        int64  `toml:"min"`
 	Max        int64  `toml:"max"`
+}
+
+type SendBackConfig struct {
+	Enable     bool   `toml:"enable"`
+	PrivateKey string `toml:"private_key"`
+	Recipient  string `toml:"recipient"`
+	Value      int64  `toml:"fixedValue"`
+	GasLimit   uint64 `toml:"gas_limit"`
+	Memo       string `toml:"memo"`
+	TxCount    int    `toml:"txCount"`
+	InTime     string `toml:"inTime"`
 }
