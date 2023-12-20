6943e680b80eab8f6676bdf3d430dfd661b9363d
Ducca <64695363+duccas@users.noreply.github.com>
2023-12-20 12:29:46 +0300

    bump version

--- cmd/evm-sender/internal/var/vars.go
+++ cmd/evm-sender/internal/var/vars.go
@@ -10,9 +10,9 @@ var (
 	InfoLog        = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	ErrorLog       = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	Binary         = "evm-sender"
-	Version        = "1.0.0"
+	Version        = "1.2.0"
 	ConfigPath     = ".evm-sender/"
 	ConfigFilePath = ".evm-sender/config.toml"
-	MissingTicker  = time.NewTicker(20 * time.Second)
+	SendBackTicker = time.NewTicker(10 * time.Second)
 	//TpsTicker      = time.NewTicker(60 * time.Second)
 )
