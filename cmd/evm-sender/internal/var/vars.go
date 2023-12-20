49982426daf864395e2ca4574f6e700a402e6e91
Ducca <64695363+duccas@users.noreply.github.com>
2023-12-20 19:37:16 +0300

    bump

--- cmd/evm-sender/internal/var/vars.go
+++ cmd/evm-sender/internal/var/vars.go
@@ -3,15 +3,14 @@ package vars
 import (
 	"log"
 	"os"
-	"time"
 )
 
 var (
 	InfoLog        = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	ErrorLog       = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	Binary         = "evm-sender"
-	Version        = "1.2.1"
+	Version        = "1.2.2"
 	ConfigPath     = ".evm-sender/"
 	ConfigFilePath = ".evm-sender/config.toml"
-	SendBackTicker = time.NewTicker(2 * time.Second)
+	//SendBackTicker = time.NewTicker(2 * time.Second)
 )
