219e8251292b18cd097f13a59b0a1b4122f83dc3
Ducca <support@staketab.com>
2024-01-10 03:29:41 +0300

    bump version

--- cmd/evm-sender/internal/var/vars.go
+++ cmd/evm-sender/internal/var/vars.go
@@ -9,7 +9,7 @@ var (
 	InfoLog        = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	ErrorLog       = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	Binary         = "evm-sender"
-	Version        = "1.2.2"
+	Version        = "1.2.3-timers"
 	ConfigPath     = ".evm-sender/"
 	ConfigFilePath = ".evm-sender/config.toml"
 	//SendBackTicker = time.NewTicker(2 * time.Second)
